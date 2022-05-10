372c60e42bc8504808237fbff8c40e22301f2357
AbdelrhmanRady <bedo8800@hotmail.com>
2022-05-11 01:28:41 +0200

    added lcd driver

--- controller.c
+++ controller.c
@@ -1,8 +1,68 @@
-#include "TM4C123.h"		// Device header
+#include "TM4C123.h"    // Device header
 #include "tm4c123gh6pm.h"
-#include "stdio.h"
 #include "string.h"
+#define LCD GPIOB            //LCD port with Tiva C 
+#define RS 0x01                     //RS -> PB0 (0x01)
+#define RW 0x02         //RW -> PB1 (0x02)
+#define EN 0x04                    //EN -> PB2 (0x04)
+void LCD4bits_Init(void);                                                     //Initialization of LCD Dispaly
+void LCD_Write4bits(unsigned char, unsigned char); //Write data as (4 bits) on LCD
+void LCD_WriteString(char*);                                             //Write a string on LCD 
+void LCD4bits_Cmd(unsigned char);                                     //Write command 
+void LCD4bits_Data(unsigned char);                                 //Write a character
 
 int main(){
-	test ya sa7by
+	
+}
+void LCD4bits_Init(void)
+{
+	SYSCTL->RCGCGPIO |= 0x02;    //enable clock for PORTB
+	//delayMs(10);                 //delay 10 ms for enable the clock of PORTB
+  LCD->DIR = 0xFF;             //let PORTB as output pins
+	LCD->DEN = 0xFF;             //enable PORTB digital IO pins
+	LCD4bits_Cmd(0x28);          //2 lines and 5x7 character (4-bit data, D4 to D7)
+	LCD4bits_Cmd(0x06);          //Automatic Increment cursor (shift cursor to right)
+	LCD4bits_Cmd(0x01);					 //Clear display screen
+	LCD4bits_Cmd(0x0F);          //Display on, cursor blinking
+}
+
+
+void LCD_Write4bits(unsigned char data, unsigned char control)
+{
+	data &= 0xF0;                       //clear lower nibble for control 
+	control &= 0x0F;                    //clear upper nibble for data
+	LCD->DATA = data | control;         //Include RS value (command or data ) with data 
+	LCD->DATA = data | control | EN;    //pulse EN
+	//delayUs(0);													//delay for pulsing EN
+	LCD->DATA = data | control;					//Turn off the pulse EN
+	LCD->DATA = 0;                      //Clear the Data 
+}
+
+void LCD_WriteString(char * str)
+{  
+	volatile int i = 0;          //volatile is important 
+	
+	while(*(str+i) != '\0')       //until the end of the string
+	{
+		LCD4bits_Data(*(str+i));    //Write each character of string
+		i++;                        //increment for next character
+	}
+}
+
+void LCD4bits_Cmd(unsigned char command)
+{
+	LCD_Write4bits(command & 0xF0 , 0);    //upper nibble first
+	LCD_Write4bits(command << 4 , 0);			 //then lower nibble
+	
+	//if(command < 4)
+		//delayMs(2);       //commands 1 and 2 need up to 1.64ms
+	//else
+		//delayUs(40);      //all others 40 us
+}
+
+void LCD4bits_Data(unsigned char data)
+{
+	LCD_Write4bits(data & 0xF0 , RS);   //upper nibble first
+	LCD_Write4bits(data << 4 , RS);     //then lower nibble
+	//delayUs(40);												//delay for LCD (MCU is faster than LCD)
 }
