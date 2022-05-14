fd438aa0d89e24cb332e4e4f98406bd28d8ed6cd
islam zidan <eslamalalamy@gmail.com>
2022-05-14 20:16:28 +0200

    updated startup file to support interrupts

--- startup_TM4C123.s
+++ startup_TM4C123.s
@@ -230,7 +230,10 @@ __Vectors_Size  EQU     __Vectors_End - __Vectors
 
 Reset_Handler   PROC
                 EXPORT  Reset_Handler             [WEAK]
+                ;IMPORT  SystemInit
                 IMPORT  __main
+                ;LDR     R0, =SystemInit
+                ;BLX     R0
                 LDR     R0, =__main
                 BX      R0
                 ENDP
@@ -928,7 +931,7 @@ PWM1_FAULT_Handler\
                 PROC
                 EXPORT  PWM1_FAULT_Handler [WEAK]
                 B       .
-                ENDP
+                ENDP                               
 
                 ALIGN
 
@@ -957,5 +960,59 @@ __user_initial_stackheap
 
                 ENDIF
 
+;******************************************************************************
+;
+; Useful functions.
+;
+;******************************************************************************
+        EXPORT  DisableInterrupts
+        EXPORT  EnableInterrupts
+        EXPORT  StartCritical
+        EXPORT  EndCritical
+        EXPORT  WaitForInterrupt
 
-                END
+;*********** DisableInterrupts ***************
+; disable interrupts
+; inputs:  none
+; outputs: none
+DisableInterrupts
+        CPSID  I
+        BX     LR
+
+;*********** EnableInterrupts ***************
+; disable interrupts
+; inputs:  none
+; outputs: none
+EnableInterrupts
+        CPSIE  I
+        BX     LR
+
+;*********** StartCritical ************************
+; make a copy of previous I bit, disable interrupts
+; inputs:  none
+; outputs: previous I bit
+StartCritical
+        MRS    R0, PRIMASK  ; save old status
+        CPSID  I            ; mask all (except faults)
+        BX     LR
+
+;*********** EndCritical ************************
+; using the copy of previous I bit, restore I bit to previous value
+; inputs:  previous I bit
+; outputs: none
+EndCritical
+        MSR    PRIMASK, R0
+        BX     LR
+
+;*********** WaitForInterrupt ************************
+; go to low power mode while waiting for the next interrupt
+; inputs:  none
+; outputs: none
+WaitForInterrupt
+        WFI
+        BX     LR
+
+
+
+    ALIGN
+    END
